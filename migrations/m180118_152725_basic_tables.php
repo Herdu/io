37316a37d4a17218dc0d9afdc985b3796613fa59
Mateusz <mateusz.dobrowolski71@gmail.com>
2018-01-23 14:48:40 +0100

    migration with basic tables

--- migrations/m180118_152725_basic_tables.php
+++ migrations/m180118_152725_basic_tables.php
@@ -12,22 +12,17 @@ class m180118_152725_basic_tables extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('furniture_type',[
-                'id' => $this->primaryKey(11),
-                'name' => $this->string(128)->notNull(),
-            ]
-            );
-
-        $this->createTable('furniture_style',[
-            'id' => $this->primaryKey(11),
-            'name' => $this->string(128)->notNull(),
-        ]);
 
         $this->createTable('furniture_type',[
                 'id' => $this->primaryKey(11),
                 'name' => $this->string(128)->notNull(),
             ]
         );
+
+        $this->createTable('furniture_style',[
+            'id' => $this->primaryKey(11),
+            'name' => $this->string(128)->notNull(),
+        ]);
 
         $this->createTable('furniture', [
             'id' => $this->primaryKey(11),
@@ -37,8 +32,12 @@ class m180118_152725_basic_tables extends Migration
             'description' => $this->string(1024)->notNull(),
             'is_renovated' => 'tinyInt(1) NOT NULL',
             'furniture_type_id' => $this->integer(11)->notNull(),
-            'furniture_stype_id' => $this->integer(11)->notNull(),
+            'furniture_style_id' => $this->integer(11)->notNull(),
         ]);
+
+        $this->addForeignKey('fk_furniture_furniture_type', 'furniture', 'furniture_type_id', 'furniture_type', 'id');
+        $this->addForeignKey('fk_furniture_furniture_style', 'furniture', 'furniture_style_id', 'furniture_style', 'id');
+
 
         $this->createTable('content', [
             'id' => $this->primaryKey(),
@@ -48,32 +47,27 @@ class m180118_152725_basic_tables extends Migration
         ]);
 
         $this->createTable('message_title',[
-           'id' => $this->primaryKey(11),
-           'name' => $this->string(128)->notNull(),
+            'id' => $this->primaryKey(11),
+            'name' => $this->string(128)->notNull(),
         ]);
 
         $this->createTable('message', [
             'id' => $this->primaryKey(11),
-            'email' => $this->string(128),
+            'email' => $this->string(128)->notNull(),
             'message_title_id' => $this->integer(11),
             'message_title' => $this->string('128'),
             'furniture_id' => $this->integer(11),
         ]);
 
-        $this->createTable('gallery_type', [
-            'id' => $this->primaryKey(11),
-            'name' => $this->string(128)->notNull(),
-        ]);
+        $this->addForeignKey('fk_message_message_title', 'message', 'message_title_id', 'message_title', 'id');
+        $this->addForeignKey('fk_message_furniture', 'message', 'furniture_id', 'furniture', 'id');
+
 
         $this->createTable('picture',[
             'id' => $this->primaryKey(11),
             'image_url' => $this->string(128)->notNull(),
             'title' => $this->string(128)->notNull(),
-            'gallery_type_id' => $this->integer(11)->notNull(),
         ]);
-
-        //todo ADD FOREIGN KEYS AND SAFEDOWN
-
     }
 
     /**
@@ -81,7 +75,18 @@ class m180118_152725_basic_tables extends Migration
      */
     public function safeDown()
     {
+        $this->dropForeignKey('fk_message_furniture', 'message');
+        $this->dropForeignKey('fk_message_message_title', 'message');
+        $this->dropForeignKey('fk_furniture_furniture_type', 'furniture');
+        $this->dropForeignKey('fk_furniture_furniture_style', 'furniture');
 
+        $this->dropTable('picture');
+        $this->dropTable('message');
+        $this->dropTable('message_title');
+        $this->dropTable('content');
+        $this->dropTable('furniture');
+        $this->dropTable('furniture_style');
+        $this->dropTable('furniture_type');
     }
 
     /*
